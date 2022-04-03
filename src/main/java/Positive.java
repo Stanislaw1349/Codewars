bc162eab52e64696e0977b1a2f905f945e036158
Stanislaw1349 <alfstas@gmail.com>
2022-04-03 22:59:03 +0300

    add task

---
+++ src/main/java/Positive.java
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/5715eaedb436cf5606000381/train/java
+public class Positive {
+    public static int sum(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] > 0) {
+                sum += arr[i];
+            }
+        }
+        return sum;
+    }
+
+}
+
